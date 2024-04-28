f5f55232e4fceb6d54e62bc3dbf505017053c7db
ruslan <lotinsk@gmail.com>
2024-04-28 22:30:44 +0600

    feat: add burger menu

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -10,6 +10,7 @@ import {
   ShoppingCartSimple,
   User,
 } from "@phosphor-icons/react";
+import openList from "../../../public/Icons/openBurgerMenu.svg";
 import { useState } from "react";
 import { BurgerMenu } from "@/widgets/BurgerMenu";
 
@@ -20,7 +21,7 @@ const Header = () => {
   return (
     <div className={cls.header + " text-white py-5"}>
       <div className="max-w-[1270px] mx-auto px-[15px]">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between gap-[24px] items-center">
           <Link to="/" className="logo ">
             <img
               src={logo}
@@ -71,7 +72,7 @@ const Header = () => {
               </button>
             </div>
             <div className="flex items-center">
-              <div className="relative hidden md:block">
+              <div className="relative  md:block">
                 <User
                   onClick={() => setMenu((prevState) => !prevState)}
                   className="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px] cursor-pointer"
@@ -92,13 +93,22 @@ const Header = () => {
               </Link>
             </div>
           </div>
-          <List
-            className="md:hidden cursor-pointer"
-            size={24}
-            onClick={() => setBurgerMenu((prevState) => !prevState)}
-          />
           {burgerMenu ? (
-            <div className="md:hidden absolute min-w-[274px] box-border top-[66px] right-[0] z-100">
+            <img
+              src={openList}
+              alt="open list"
+              className="cursor-pointer"
+              onClick={() => setBurgerMenu((prevState) => !prevState)}
+            />
+          ) : (
+            <List
+              className="md:hidden cursor-pointer"
+              size={24}
+              onClick={() => setBurgerMenu((prevState) => !prevState)}
+            />
+          )}
+          {burgerMenu ? (
+            <div className="md:hidden absolute w-full h-full box-border top-[66px] right-[0] z-100">
               <BurgerMenu onClose={() => setMenu((prevState) => !prevState)} />
             </div>
           ) : null}
