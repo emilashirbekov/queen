8edf3977e0abd58bfbf870c4f55c2a6a23fbe0cb
sanjar <san4o.myrzakmatov@gmail.com>
2024-04-28 22:53:32 +0600

    seo

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,22 +1,24 @@
-import { FAQ } from '@/widgets/FAQ';
+import {FAQ} from '@/widgets/FAQ';
 import CategorySection from './MainSections/CategorySection';
 import CollectionSection from './MainSections/CollectionSection';
 import HeroSections from './MainSections/HeroSections';
 import OfferSection from './MainSections/OfferSection';
 import RecomendationSection from './MainSections/RecomendationSection';
+import MainHelmet from "@/pages/Helmet/MainHelment";
 //import Modal from '@/widgets/Modal/Modal';
 //import { Link } from 'react-router-dom';
 
 const MainPage = () => {
     return (
         <>
+            <MainHelmet/>
             <main className="flex flex-col max-container gap-[80px]">
-                <HeroSections />
-                <CategorySection />
-                <OfferSection />
-                <RecomendationSection />
-                <CollectionSection />
-                <FAQ />
+                <HeroSections/>
+                <CategorySection/>
+                <OfferSection/>
+                <RecomendationSection/>
+                <CollectionSection/>
+                <FAQ/>
             </main>
             {/*<Modal className="bg-white w-[300px] h-[300px] flex flex-col p-[20px] gap-[10px] rounded-3xl">
                 <h1 className="text-center">Text</h1>
