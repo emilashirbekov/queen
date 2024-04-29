3cf0f23b03eb227b41dbbee4f205d98b1878d169
Emka Ashirbekov <emkaashirbekov7910a@gmail.com>
2024-04-29 11:01:30 +0600

    changed text in faq

--- src/shared/ui/Accordion.tsx
+++ src/shared/ui/Accordion.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
 import { Minus, Plus } from '@phosphor-icons/react';
+import React, { useState } from 'react';
 
 interface AccordionProps {
     desc: string;
+    label: string;
 }
 
-const Accordion: React.FC<AccordionProps> = ({ desc }) => {
+const Accordion: React.FC<AccordionProps> = ({ desc, label }) => {
     const [isActive, setIsActive] = useState(false);
 
     const toggle = () => {
@@ -15,7 +16,7 @@ const Accordion: React.FC<AccordionProps> = ({ desc }) => {
     return (
         <li className="mb-8">
             <p onClick={toggle} className="flexBetween cursor-pointer">
-                <span className="text-base sm:text-xl font-semibold mb-3">Описание</span>
+                <span className="w-fit text-sm sm:text-xl font-semibold mb-3">{ label}</span>
                 {isActive ? <Minus size={24} /> : <Plus size={24} />}
             </p>
             <p
