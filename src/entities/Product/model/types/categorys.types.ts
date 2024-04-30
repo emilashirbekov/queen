11274270ec061f0ddbb7b673352593ad6e905a30
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-04-30 23:03:27 +0600

    fix favourite

--- src/entities/Product/model/types/categorys.types.ts
+++ src/entities/Product/model/types/categorys.types.ts
@@ -1,16 +1,17 @@
 export interface CategorysTypes {
-    id: number;
-    title: string;
-    name?: string;
-    price?: number;
-    image: string;
-    image1?: string;
-    images1?: string;
+  id: number;
+  title: string;
+  name?: string;
+  price?: number;
+  image: string;
+  image1?: string;
+  images1?: string;
+  is_favorite: { id: number; user: number }[];
 }
 
 export interface CategoriesApiResponse {
-    count: number;
-    next: string | null;
-    previous: string | null;
-    results: CategorysTypes[];
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: CategorysTypes[];
 }
