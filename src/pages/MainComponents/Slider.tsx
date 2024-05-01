9125cfd4167a0ec038718bf40cd6bd52db292223
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-05-01 23:49:32 +0600

    merge

--- src/pages/MainComponents/Slider.tsx
+++ src/pages/MainComponents/Slider.tsx
@@ -1,5 +1,5 @@
+import React from "react";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "swiper/css";
 import "swiper/css/pagination";
