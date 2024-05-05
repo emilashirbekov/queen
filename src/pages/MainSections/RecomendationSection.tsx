0252ea6d54900a3871f67a58ccc2512426cc290e
Emka Ashirbekov <emkaashirbekov7910a@gmail.com>
2024-05-05 16:11:34 +0600

    fixed confl

--- src/pages/MainSections/RecomendationSection.tsx
+++ src/pages/MainSections/RecomendationSection.tsx
@@ -53,26 +53,22 @@ const RecomendationSection = () => {
           Наши рекомендации
         </h1>
       </div>
-      {data.length !== 0 ? (
-        <Slider
-          slidesPerView={1}
-          spaceBetween={30}
-          className="h-full relative"
-          slideClassName=" h-full flex flex-col items-center gap-[16px] cursor-pointer relative"
-          imageClassName="w-full h-full min-h-[180px] min-w-[180px] max-h-[310px] object-cover bg-white rounded-[20px]"
-          textClassName="w-full"
-          favorite={true}
-          data={data}
-          //break0={1}
-          break500={2}
-          break768={2}
-          break1024={4}
-          break1366={4}
-          break2560={5}
-        />
-      ) : (
-        <div className="text-[26px] text-center">No data(</div>
-      )}
+      <Slider
+        slidesPerView={1}
+        spaceBetween={30}
+        className="h-full relative"
+        slideClassName=" h-full flex flex-col items-center gap-[16px] cursor-pointer relative"
+        imageClassName="w-full h-full min-h-[180px] min-w-[180px] max-h-[310px] object-cover bg-white rounded-[20px]"
+        textClassName="w-full"
+        favorite={true}
+        data={products}
+        //break0={1}
+        break500={2}
+        break768={2}
+        break1024={4}
+        break1366={4}
+        break2560={5}
+      />
     </section>
   );
 };
