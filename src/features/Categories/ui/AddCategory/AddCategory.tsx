c6ab37bc9d0a87ccf902e877d2866d93f2577f3c
Emka Ashirbekov <emkaashirbekov7910a@gmail.com>
2024-05-05 15:42:49 +0600

    cateogry changes

--- src/features/Categories/ui/AddCategory/AddCategory.tsx
+++ src/features/Categories/ui/AddCategory/AddCategory.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { AddCategoryForm } from "@/entities/Categories";
 import { useCreateCategoryMutation } from "@/features/Categories/ui/model/services/categoriesAPI";
 import SuccessErrorMessage from "@/shared/ui/SuccessErrorMessage";
