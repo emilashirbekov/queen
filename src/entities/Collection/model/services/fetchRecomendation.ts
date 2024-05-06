007be58c9f5d356400507e65ffce12d3182c4e5d
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-05-07 00:59:58 +0600

    added reco in admin

--- src/entities/Collection/model/services/fetchRecomendation.ts
+++ src/entities/Collection/model/services/fetchRecomendation.ts
@@ -1,41 +1,48 @@
-import { BASE_URL } from '@/app/constants/contants'
-import { RootState } from '@/app/providers/StoreProvider/config/store'
-import { createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
-import { axiosApi } from '@/app/providers/http/axiosApi'
-import { ListResponse } from '@/app/types/types'
-import { RecomendationApi } from '../slice/recomendationSlice'
+import { BASE_URL } from "@/app/constants/contants";
+import { RootState } from "@/app/providers/StoreProvider/config/store";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { axiosApi } from "@/app/providers/http/axiosApi";
+import { ListResponse } from "@/app/types/types";
+import { RecomendationApi } from "../slice/recomendationSlice";
 
 export const fetchRecomendation = createAsyncThunk<
-	ListResponse<RecomendationApi>[],
-	void,
-	{ state: RootState }
->('products/fetchRecomendation', async () => {
-	const response = await axios.get(
-		`${BASE_URL}/collection/list/recommendation/`
-	)
-	return response.data.results
-})
+  ListResponse<RecomendationApi>[],
+  void,
+  { state: RootState }
+>("products/fetchRecomendation", async () => {
+  const response = await axios.get(
+    `${BASE_URL}/collection/list/recommendation/`,
+  );
+  return response.data.results;
+});
 
 export const addRecomendation = createAsyncThunk<void, number[]>(
-	'collection/createListCollection',
-	async product => {
-		await axiosApi.post('/collection/create/recommendation/', {
-			product,
-		})
-	}
-)
+  "collection/createListCollection",
+  async (product) => {
+    await axiosApi.post("/collection/create/recommendation/", {
+      product,
+    });
+  },
+);
 
 interface UpdateRecomendation {
-	id: number
-	product: number[]
+  id: number;
+  product: number[];
 }
 
 export const updateRecomendation = createAsyncThunk<void, UpdateRecomendation>(
-	'recomendation/updateListRecomendation',
-	async ({ id, product }) => {
-		await axiosApi.put(`/collection/rud/recommendation/${id}/`, {
-			product: product,
-		})
-	}
-)
+  "recomendation/updateListRecomendation",
+  async ({ id, product }) => {
+    await axiosApi.put(`/collection/rud/recommendation/${id}/`, {
+      product: product,
+    });
+  },
+);
+
+export const deleteRecommendation = createAsyncThunk<void, number>(
+  "recommendation/deleteReco",
+  async (id) => {
+    await axiosApi.delete(`/collection/rud/recommendation/${id}/`);
+  },
+);
